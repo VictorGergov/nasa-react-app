d0bed8ebf5c37f6bb5122a9d00d76cda19219a75
VictorGergov <vgergov9@gmail.com>
2025-01-08 16:53:41 +0200

    Making first part of the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+import SideBar from "./components/SideBar"
+
+function App() {
+
+  return (
+    <>
+      <SideBar></SideBar>
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
